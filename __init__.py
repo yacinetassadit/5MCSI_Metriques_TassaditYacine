0553e4649e0a2f6746c20832a3b32e38d250eefe
yacinetassadit <114904393+yacinetassadit@users.noreply.github.com>
2024-02-12 18:59:20 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ def hello_world():
 def contact():
     return render_template("Contact.html")
 
-@app.route('/paris/') #problème de token donc ça ne fonctionne pas!
+@app.route('/paris/') 
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
