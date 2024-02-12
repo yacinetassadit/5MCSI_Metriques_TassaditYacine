86193abbd4520130a96ebc48c3b708ec4affaf47
yacinetassadit <114904393+yacinetassadit@users.noreply.github.com>
2024-02-12 18:38:09 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -18,13 +18,13 @@ def contact():
 
 @app.route('/paris/') #problème de token donc ça ne fonctionne pas!
 def meteo():
-    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
