805ec57970181a7b13010af2daec55871a51bb96
yacinetassadit <114904393+yacinetassadit@users.noreply.github.com>
2024-02-12 16:25:12 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
+#from flask import render_template
 from flask import json
 from datetime import datetime
 from urllib.request import urlopen
